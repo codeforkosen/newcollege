c062d37b142d466c6c9595bf02278d1c47b39d9b
Taisuke Fukuno <fukuno@jig.jp>
2022-09-01 16:10:21 +0900

    fix json

--- makejson.js
+++ makejson.js
@@ -3,7 +3,7 @@ import { CSV } from "https://js.sabae.cc/CSV.js";
 const data = CSV.toJSON(await CSV.fetch("newcollege202208.csv"));
 const notes = CSV.toJSON(await CSV.fetch("newcollege202208-notes.csv"));
 data.forEach(d => {
-  d.附帯事項 = notes.filter(n => n.設置予定大学等 == d.設置予定大学等).map(n => {
+  d.附帯事項 = notes.filter(n => n.開設予定大学等 == d.開設予定大学等).map(n => {
     return { 附帯事項: n.附帯事項, 附帯事項区分: n.附帯事項区分 };
   });
 });
